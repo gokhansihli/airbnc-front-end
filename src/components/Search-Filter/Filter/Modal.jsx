286739cac6ac9c25269e073ae8033c0d6dbd4786
Gökhan Sihli <gokhansihli@Mac.lan>
2025-12-21 21:50:47 +0000

    Added Escape key to modal

--- src/components/Search-Filter/Filter/Modal.jsx
+++ src/components/Search-Filter/Filter/Modal.jsx
@@ -1,8 +1,20 @@
 import { createPortal } from "react-dom";
 import "./modal.css";
+import { useEffect } from "react";
 
 export default function Modal({ children, onClick, maxWidth }) {
   const modalRoot = document.getElementById("modal-root");
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        onClick();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => removeEventListener("keydown", handleKeyDown);
+  }, [onclick]);
 
   return createPortal(
     <div className="modal-overlay" onClick={onClick}>
